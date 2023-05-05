348bc6396e7fc39727cd72fa1cce7b54c5a5a3b0
solahidris <emailsolah@gmail.com>
2023-05-06 00:11:28 +0800

    add css flex

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='App-Description'>
+          1- Create a fully responsive navbar on React<br/>
+          1.5- using CSS grid and flexbox.<br/>
+          2- Additionally, display breadcrumbs with the menu dropdown<br/>
+          2.5- on mobile screen size.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>----------------------------</p>
+        <div className='NavbarDiv'>
+          <ul className='NavbarUL'>
+            <li><a href="locasthost:3000">Home</a></li>
+            <li>About</li>
+            <li>News</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </header>
     </div>
   );
